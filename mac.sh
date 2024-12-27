11f9ee3804eda9fd0bf0e6a7a9a3bef0669ff514
tinntvs <87159604+tinntvs@users.noreply.github.com>
2024-12-27 19:17:51 +0700

    Update mac.sh
    Change some functions

--- mac.sh
+++ mac.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
-# Prompt for domain user and passwords
-read -p "Enter the domain username: " domainuser
-read -sp "Enter the domain user password: " domainuserpassword
-echo
-read -p "Enter the local admin username: " localadmin
-echo
-read -sp "Enter the local admin password: " localadminpassword
-echo
-
-#Disabling Secure Token
-sudo sysadminctl -secureTokenOff $domainuser -password $domainuserpassword -adminUser $localadmin -adminPassword $localadminpassword >> /tmp/disable.log
-if [ $? -eq 0 ]; then
-    echo "Secure token is disabled for $domainuser - enabling the Secure Token."
-    #Enable Secure Token
-    sudo sysadminctl -secureTokenOn $domainuser -password $domainuserpassword -adminUser $localadmin -adminPassword $localadminpassword  >> /tmp/enable.log
+prompt_credentials() {
+    read -p "Enter the domain username: " domainuser
+    read -sp "Enter the domain user password: " domainuserpassword
+    echo
+    read -sp "Enter the local admin username: " localadmin
+    echo
+    read -sp "Enter the local admin password: " localadminpassword
+    echo
+}
+secure_token_operations()
+{
+    operation=$1
+    sudo sysadminctl -${operation} $domainuser -password $domainuserpassword -adminUser $localadmin -adminPassword $localadminpassword >> "/tmp/${operation}.log"
     if [ $? -eq 0 ]; then
-        echo "Secure token is enabled for $domainuser - Let's restart your device and select  CREATE A NEW KEYCHAIN - option"
+        echo "Secure token operation '${operation}' successful for $domainuser."
     else
-        echo "Failed - Showing the logs"
+        echo "Failed to perform secure token operation '${operation}'. Showing the logs:"
         echo "================================================================"
-        cat /tmp/enable.log
+        cat "/tmp/${operation}.log"
         exit 1
-    fi
-else
-    echo "Failed to disable secure token."
-    echo "================================================================"
-    cat /tmp/disable.log
-    exit 1
-fi
+}
+prompt_credentials
+secure_token_operations "secureTokenOff"
+secure_token_operations "secureTokenOn"
+echo "Secure token operations completed successfully. Restart your device and select 'CREATE A NEW KEYCHAIN' option."
